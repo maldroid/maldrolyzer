dd37e71d93359d63a2e8d1b9cb552c21c128e528
Zenobiusz <maldroid@users.noreply.github.com>
2015-03-30 23:06:22 +0200

    xbot007 plugin now covers the '%' version

--- plugins/xbot007.py
+++ plugins/xbot007.py
@@ -5,8 +5,12 @@ class Xbot007(Plugin):
 
 	def recon(self):
 		for s in self.dvm.get_strings():
-			if 'xbot007' in s.lower().translate(None, '#%'):
-                                return True
+			if 'xbot007' in s.lower().translate(None, '#'):
+				self.version = '#'
+				return True
+			elif 'xbot007' in s.lower().translate(None, '%'):
+				self.version = '%'
+				return True
                 return False
 
 	def extract(self):
@@ -14,6 +18,9 @@ class Xbot007(Plugin):
 		for string in self.dvm.get_strings():
 			if string.endswith('.php'):
 				php_end = string
+		if self.version == '%':
+				host = [self.apk.get_android_resources().get_string(self.apk.get_package(), 'domain')[1],
+					self.apk.get_android_resources().get_string(self.apk.get_package(), 'domain2')[1]]
 		for cls in self.dvm.get_classes():
 			# There has to be a better method to do THIS
 			if len(cls.get_methods()) == 1 and\
@@ -22,7 +29,7 @@ class Xbot007(Plugin):
 				len(cls.get_fields()) < 10:
 				for inst in cls.get_methods()[0].get_instructions():
 					if inst.get_name() == 'const-string':
-						host = inst.get_output().translate(None, '#%').split(',')[-1].strip("' ")
+						host = [inst.get_output().translate(None, '#%').split(',')[-1].strip("' ")]
 						break
-		result = {'c2': ['http://' + host + '/' + php_end]}
+		result = {'c2': map(lambda h: ('http://' + h + '/' + php_end), host)}
 		return result
